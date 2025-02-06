dcb0bb1d2b75762a48f3c26e961889136403f80e
rohankbd <rohankbd.1@gmail.com>
2025-02-07 00:25:51 +0530

    Updated redis config

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,16 +1,18 @@
 import Redis from "ioredis";
 
-// LOCAL
-// const redisClient = new Redis({
-//   host: process.env.REDIS_HOST || "localhost",
-//   port: process.env.REDIS_PORT || 6379,
-// });
+let redisClient;
 
-// PRODUCTION
-const redisClient = new Redis(process.env.REDIS_URL);
+if (process.env.NODE_ENV === "production") {
+    redisClient = new Redis(process.env.REDIS_URL);
+} else {
+    redisClient = new Redis({
+        host: process.env.REDIS_HOST || "localhost",
+        port: process.env.REDIS_PORT || 6379
+    });
+}
 
 redisClient.on("error", (err) => {
-  console.error("Redis Client Error", err);
+    console.error("Redis Client Error", err);
 });
 
 export default redisClient;
